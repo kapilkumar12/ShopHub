41d151ca2b46dc2599e945f2beb37e2b67b430b3
kapilkumar12 <kapilkumar1492003@gmail.com>
2026-05-04 15:29:21 +0530

    live api

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,7 @@
 // services/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("https://shophub-backend-5fs0.onrender.com", {
+const socket = io("https://shophub-backend-ee64.onrender.com", {
   withCredentials: true,
 });
 
