10b5798fa2972c8451f17c9366f5b799cf91a154
kapilkumar12 <kapilkumar1492003@gmail.com>
2026-05-06 13:11:53 +0530

    resolve skeleton problem

--- src/skeletons/ProductCartSkeleton.jsx
+++ src/skeletons/ProductCartSkeleton.jsx
@@ -1,13 +1,17 @@
-export default function ProductCartSkeleton() {
-    return Array(6)
-    .fill(0)
-    .map((_,i) => (
-      <div key={i} className="bg-white p-3 rounded-xl shadow animate-pulse">
-        <div className="h-48 bg-gray-300 rounded mb-3"></div>
-        <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
-        <div className="h-3 bg-gray-300 rounded w-full mb-2"></div>
-        <div className="h-6 bg-gray-300 rounded w-1/3"></div>
-      </div>
-    ));
+export default function ProductCardSkeleton({ count = 6 }) {
+  return (
+    <>
+      {Array.from({ length: count }).map((_, i) => (
+        <div
+          key={i}
+          className="bg-white p-3 rounded-xl shadow animate-pulse"
+        >
+          <div className="h-48 bg-gray-300 rounded mb-3"></div>
+          <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
+          <div className="h-3 bg-gray-200 rounded w-full mb-2"></div>
+          <div className="h-6 bg-gray-300 rounded w-1/3"></div>
+        </div>
+      ))}
+    </>
+  );
 }
-
